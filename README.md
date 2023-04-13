c34dfe18246f9801744d930adec10d2e23a5af13
DB-cenos <db@cenos-platform.com>
2023-04-13 10:23:33 +0300

    Initial solvers

---
+++ README.md
@@ -0,0 +1,12 @@
+# MHD solvers
+* To install all solvers, just execute ./Allwmake in a bash terminal of a properly prepared Linux environment.
+
+## buoyantFoamMHD##
+* Solver for steady or transient buoyant, turbulent flow of compressible fluids for electromagnetically forced and heated flows.
+
+## pimpleFoamMHD##
+* Transient solver for electromagnetically forced incompressible, turbulent flow of Newtonian fluids.
+
+## simpleFoamMHD##
+* Steady-state solver for electromagnetically forced incompressible, turbulent flow.
+
---
+++ pimpleFoamMHD/pimpleFoam.C
@@ -0,0 +1,205 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     | Website:  https://openfoam.org
+    \\  /    A nd           | Copyright (C) 2011-2022 OpenFOAM Foundation
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+License
+    Original pimpleFoam solver is part of OpenFOAM.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+Application
+    pimpleFoamMHD is a modified pimpleFoam solver, where the modification 
+    is based on EOF-Library solver mhdVxBPimpleFoam.
+
+Description
+    Transient solver for electromagnetically forced incompressible, 
+    turbulent flow of Newtonian fluids, with optional mesh motion and mesh 
+    topology changes.
+
+    Turbulence modelling is generic, i.e. laminar, RAS or LES may be selected.
+
+\*---------------------------------------------------------------------------*/
+
+#include "fvCFD.H"
+#include "viscosityModel.H"
+#include "incompressibleMomentumTransportModels.H"
+#include "pimpleControl.H"
+#include "pressureReference.H"
+#include "CorrectPhi.H"
+#include "fvModels.H"
+#include "fvConstraints.H"
+#include "localEulerDdtScheme.H"
+#include "fvcSmooth.H"
+#include "Elmer.H"
+
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+int main(int argc, char *argv[])
+{
+    #include "postProcess.H"
+
+    #include "setRootCaseLists.H"
+    #include "createTime.H"
+    #include "createMesh.H"
+    #include "initContinuityErrs.H"
+    #include "createDyMControls.H"
+    #include "createFields.H"
+    #include "createUfIfPresent.H"
+
+    turbulence->validate();
+
+    if (!LTS)
+    {
+        #include "CourantNo.H"
+        #include "setInitialDeltaT.H"
+    }
+
+    // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+    Info<< "\nStarting time loop\n" << endl;
+
+    // Send fields to Elmer
+    Elmer<fvMesh> sending(mesh,1); // 1=send, -1=receive
+    sending.sendStatus(1); // 1=ok, 0=lastIter, -1=error
+    sending.sendVector(U);
+
+    // Receive fields from Elmer
+    Elmer<fvMesh> receiving(mesh,-1); // 1=send, -1=receive
+    receiving.sendStatus(1); // 1=ok, 0=lastIter, -1=error
+    receiving.recvVector(JxB);
+
+    while (pimple.run(runTime))
+    {
+        #include "readDyMControls.H"
+
+        if (LTS)
+        {
+            #include "setRDeltaT.H"
+        }
+        else
+        {
+            #include "CourantNo.H"
+            #include "setDeltaT.H"
+        }
+
+        fvModels.preUpdateMesh();
+
+        // Update the mesh for topology change, mesh to mesh mapping
+        mesh.update();
+
+        runTime++;
+
+        Info<< "Time = " << runTime.userTimeName() << nl << endl;
+
+        // --- Pressure-velocity PIMPLE corrector loop
+        while (pimple.loop())
+        {
+            if (pimple.firstPimpleIter() || moveMeshOuterCorrectors)
+            {
+                // Move the mesh
+                mesh.move();
+
+                if (mesh.changing())
+                {
+                    MRF.update();
+
+                    if (correctPhi)
+                    {
+                        #include "correctPhi.H"
+                    }
+
+                    if (checkMeshCourantNo)
+                    {
+                        #include "meshCourantNo.H"
+                    }
+                }
+            }
+
+            fvModels.correct();
+
+            #include "UEqn.H"
+
+            // --- Pressure corrector loop
+            while (pimple.correct())
+            {
+                #include "pEqn.H"
+            }
+
+            if (pimple.turbCorr())
+            {
+                viscosity->correct();
+                turbulence->correct();
+            }
+        }
+
+        runTime.write();
+
+        Info<< "ExecutionTime = " << runTime.elapsedCpuTime() << " s"
+            << "  ClockTime = " << runTime.elapsedClockTime() << " s"
+            << nl << endl;
+			
+        // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+        dimensionedScalar smallU
+        (
+            "smallU",
+            dimensionSet(0, 1, -1, 0, 0, 0 ,0),
+            1e-6
+        );
+
+        // Check whether we need to update electromagnetic stuff with Elmer
+        scalar maxRelDiff_local = (max(mag(U_old-U)/(average(mag(U))+smallU))).value();
+
+        bool doElmer = false;
+        if(maxRelDiff_local>maxRelDiff && (maxRelDiff<SMALL || maxRelDiff+SMALL<=1.0)) {
+            doElmer = true;
+        }
+
+        if(doElmer && runTime.run()) {
+            U_old = U;
+
+            // Send fields to Elmer
+            sending.sendStatus(1);
+            sending.sendVector(U);
+
+            // Receive fields form Elmer
+            receiving.sendStatus(1);
+    	    receiving.recvVector(JxB);
+        }
+    }
+
+    Info << "Final iteration: "
+        << "ExecutionTime = " << runTime.elapsedCpuTime() << " s"
+        << "  ClockTime = " << runTime.elapsedClockTime() << " s" << nl << endl;
+
+    //Final iter for Elmer
+    U_old = U;
+    // Send fields to Elmer
+    sending.sendStatus(0);
+    sending.sendVector(U);
+    // Receive fields form Elmer
+    receiving.sendStatus(0);
+   	receiving.recvVector(JxB);
+
+    Info<< "End\n" << endl;
+
+    return 0;
+}
+
+
+// ************************************************************************* //
