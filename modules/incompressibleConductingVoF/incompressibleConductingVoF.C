ccedab2c4e09c58f077982edee3086fdd525ae47
cenos <cenos@DESKTOP-O8OB5OL.localdomain>
2025-11-12 17:22:33 +0200

    incompressibleConductingVoF sketch

---
+++ modules/incompressibleConductingVoF/incompressibleConductingVoF.C
@@ -0,0 +1,180 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     | Website:  https://openfoam.org
+    \\  /    A nd           | Copyright (C) 2022-2023 OpenFOAM Foundation
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+\*---------------------------------------------------------------------------*/
+
+#include "incompressibleConductingVoF.H"
+#include "localEulerDdtScheme.H"
+#include "fvCorrectPhi.H"
+#include "geometricZeroField.H"
+#include "addToRunTimeSelectionTable.H"
+
+// * * * * * * * * * * * * * * Static Data Members * * * * * * * * * * * * * //
+
+namespace Foam
+{
+namespace solvers
+{
+    defineTypeNameAndDebug(incompressibleConductingVoF, 0);
+    addToRunTimeSelectionTable(solver, incompressibleConductingVoF, fvMesh);
+}
+}
+
+
+// * * * * * * * * * * * * * * * * Constructors  * * * * * * * * * * * * * * //
+
+Foam::solvers::incompressibleConductingVoF::incompressibleConductingVoF(fvMesh& mesh)
+:
+    twoPhaseConductingVoFSolver
+    (
+        mesh,
+        autoPtr<twoPhaseVoFMixture>(new incompressibleTwoPhaseVoFMixture(mesh))
+    ),
+
+    mixture
+    (
+        refCast<incompressibleTwoPhaseVoFMixture>(twoPhaseConductingVoFSolver::mixture)
+    ),
+
+    p
+    (
+        IOobject
+        (
+            "p",
+            runTime.name(),
+            mesh,
+            IOobject::NO_READ,
+            IOobject::AUTO_WRITE
+        ),
+        p_rgh + rho*buoyancy.gh
+    ),
+
+    pressureReference_
+    (
+        p,
+        p_rgh,
+        pimple.dict()
+    ),
+
+    momentumTransport
+    (
+        U,
+        phi,
+        alphaPhi1,
+        mixture
+    )
+{
+    // Read the controls
+    readControls();
+
+    if (correctPhi || mesh.topoChanging())
+    {
+        rAU = new volScalarField
+        (
+            IOobject
+            (
+                "rAU",
+                runTime.name(),
+                mesh,
+                IOobject::READ_IF_PRESENT,
+                IOobject::AUTO_WRITE
+            ),
+            mesh,
+            dimensionedScalar(dimTime/dimDensity, 1)
+        );
+    }
+
+    if (!runTime.restart() || !divergent())
+    {
+        correctUphiBCs(U_, phi_, true);
+
+        fv::correctPhi
+        (
+            phi_,
+            U,
+            p_rgh,
+            rAU,
+            autoPtr<volScalarField>(),
+            pressureReference(),
+            pimple
+        );
+    }
+}
+
+
+// * * * * * * * * * * * * * * * * Destructor  * * * * * * * * * * * * * * * //
+
+Foam::solvers::incompressibleConductingVoF::~incompressibleConductingVoF()
+{}
+
+
+// * * * * * * * * * * * * * * Member Functions  * * * * * * * * * * * * * * //
+
+void Foam::solvers::incompressibleConductingVoF::prePredictor()
+{
+    twoPhaseConductingVoFSolver::prePredictor();
+
+    const dimensionedScalar& rho1 = mixture.rho1();
+    const dimensionedScalar& rho2 = mixture.rho2();
+
+    // Calculate the mass-flux from the accumulated alphaPhi1
+    rhoPhi = (alphaPhi1*(rho1 - rho2) + phi*rho2);
+
+    if (pimple.predictTransport())
+    {
+        momentumTransport.predict();
+    }
+}
+
+
+void Foam::solvers::incompressibleConductingVoF::pressureCorrector()
+{
+    incompressiblePressureCorrector(p);
+}
+
+
+void Foam::solvers::incompressibleConductingVoF::thermophysicalPredictor()
+{}
+
+
+void Foam::solvers::incompressibleConductingVoF::postCorrector()
+{
+    if (pimple.correctTransport())
+    {
+        momentumTransport.correct();
+    }
+    /*if (electro.correctElectromagnetics())
+    {
+        //Correct current density
+        electro_.correct();
+    }*/
+}
+
+/*void Foam::solvers::incompressibleConductingVoF::solveElectromagnetics()
+{
+    //Solve potential equation
+    electro_.solve();
+}*/
+
+
+// ************************************************************************* //
